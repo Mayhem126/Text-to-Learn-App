ed6e69ef96b1a8c720507c8f1b5d42d3a43a2d1f
Swastik <kitsaws12@gmail.com>
2026-03-24 15:58:20 +0530

    refactor: add production domain to CORS allowed origins

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
+      "https://text-to-learn.swastikdattagupta.in",
       FRONTEND_URL,
     ],
     credentials: true,
